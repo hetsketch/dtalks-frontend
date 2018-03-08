5dd9bc1518b96ea4c2b5fc8ed87091d976d44aa8
Alex Koval <al3xander.koval@gmail.com>
2018-03-08 23:36:13 +0300

    Add dropdown items to login button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,15 +4,48 @@
       class="link-text"
       v-if="!$_.isEmpty($store.state.currentUser)">
       <span slot="trigger">
-        <span>{{ $store.state.currentUser.username }}</span>
+        <span class="login__username">{{ $store.state.currentUser.username }}</span>
         <b-icon icon="chevron-down" size="is-small"></b-icon>
       </span>
-      <b-dropdown-item>Профиль</b-dropdown-item>
-      <b-dropdown-item @click="$store.dispatch('logout')">Выход</b-dropdown-item>
+
+      <b-dropdown-item>
+        <router-link
+          :to="{ name: 'user.profile', params: { id: currentUser.id }}"
+          class="has-text-black">
+          <b-icon
+            pack="far"
+            icon="user"
+            size="is-small"
+            custom-class="regular">
+          </b-icon>
+          Профиль
+        </router-link>
+      </b-dropdown-item>
+
+      <b-dropdown-item>
+        <router-link
+          :to="{ name: 'user.preferences', params: { id: currentUser.id }}"
+          class="has-text-black">
+          <b-icon
+            icon="cog"
+            size="is-small">
+          </b-icon>
+          Настройки
+        </router-link>
+      </b-dropdown-item>
+
+      <hr class="dropdown-divider">
+      <b-dropdown-item @click="$store.dispatch('logout')">
+        <b-icon
+          icon="sign-out-alt"
+          size="is-small">
+        </b-icon>
+        Выход
+      </b-dropdown-item>
     </b-dropdown>
 
     <div v-else>
-      <b-icon icon="sign-in" type="is-blue"></b-icon>
+        <b-icon icon="sign-in-alt"></b-icon>
       <span class="link-text" @click="isComponentModalActive = true">Войти</span>
     </div>
     <b-modal :active.sync="isComponentModalActive">
@@ -23,8 +56,9 @@
 </template>
 
 <script>
-  import LoginForm from './LoginForm'
-  import SignupForm from './SignupForm'
+  import LoginForm from './LoginForm';
+  import SignupForm from './SignupForm';
+  import { mapGetters } from 'vuex';
 
   export default {
     data() {
@@ -32,6 +66,10 @@
         isComponentModalActive: false,
         showLoginForm: true
       }
+    },
+    computed: {
+
+      ...mapGetters(['currentUser'])
     },
     methods: {
       toggleForm() {
@@ -48,7 +86,7 @@
 <style lang="scss">
   .login {
     .link-text {
-      margin-left: 8px
+      margin-left: 8px;
     }
 
     .modal-content {
@@ -60,6 +98,13 @@
 
     h2 {
       margin-bottom: 10px;
+    }
+
+    &__username {
+      -webkit-user-select: none; /* webkit (safari, chrome) browsers */
+      -moz-user-select: none; /* mozilla browsers */
+      -khtml-user-select: none; /* webkit (konqueror) browsers */
+      -ms-user-select: none; /* IE10+ */
     }
   }
 </style>
