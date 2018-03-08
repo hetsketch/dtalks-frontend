7c662cb4c630903695a136e894a10ad8fed236cb
Alex Koval <al3xander.koval@gmail.com>
2018-03-08 23:40:11 +0300

    Add 404 and Company pages

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <p>
+      404 page!
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,20 @@ import DiscussionBoard from './components/discussion/DiscussionBoard';
 import EventPage from './components/event/EventPage';
 import UserProfile from './components/user/UserProfile';
 import TopicPage from './components/topic/TopicPage';
-import Topic from './components/topic/Topic';
 import Event from './components/event/Event';
 import NewEvent from './components/event/NewEvent';
 import NewTopic from './components/topic/NewTopic';
+import Company from './components/company/Company';
+import NotFound from './components/NotFound';
 
 export const routes = [
-  { path: '/discussions', component: DiscussionBoard },
+  { name: 'discussions',path: '/discussions', component: DiscussionBoard },
   { name: 'topic.new', path: '/discussions/new-topic', component: NewTopic },
   { name: 'topicPage', path: '/discussions/:id', component: TopicPage },
-  { name: 'currentUser', path: '/users/:id', component: UserProfile },
-  { name: 'eventPage', path: '/events', component: EventPage },
+  { name: 'user.profile', path: '/users/:id', component: UserProfile },
+  { name: 'events', path: '/events', component: EventPage },
   { name: 'event.new', path: '/events/new-event', component: NewEvent },
-  { name: 'showEvent', path: '/events/:id', component: Event }
+  { name: 'showEvent', path: '/events/:id', component: Event },
+  { name: 'company.show', path: '/companies/:id', component: Company },
+  { name: '404', path: '*', component: NotFound },
 ];
